e39152c08ae44df090dab1fbbd6ee3838eaee87d
xiaohuo <xiaohuo@voyage.finance>
2023-06-09 20:35:55 +0800

    feat: RenkinLottery

---
+++ contracts/interface/IRandomNumberGenerator.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+interface IRandomNumberGenerator {
+    /**
+     * Requests randomness from a user-provided seed
+     */
+    function getRandomNumber(uint256 _seed) external;
+
+    /**
+     * View latest lotteryId numbers
+     */
+    function viewLatestLotteryId() external view returns (uint256);
+
+    /**
+     * Views random result
+     */
+    function viewRandomResult() external view returns (uint32);
+}
