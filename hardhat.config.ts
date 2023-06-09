07c0e4a30173f5c78796e7e6d9be4bb2282dabf1
xiaohuo <xiaohuo@voyage.finance>
2023-06-09 21:52:37 +0800

    feat: network config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,44 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { config as dotenvConfig } from "dotenv";
+
+
+dotenvConfig({ path: resolve(__dirname, "./.env") });
+import { resolve } from "path";
+
+const MAINNET_MNEMONIC = process.env.MAINNET_MNEMONIC || "";
+
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  networks: {
+    goerli: {
+        chainId: 5,
+        url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_GOERLI_API_KEY}`,
+        accounts: {
+            mnemonic: process.env.GOERLI_MNEMONIC,
+        },
+    },
+    mainnet: {
+        chainId: 1,
+        url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_MAINNET_API_KEY}`,
+        accounts: {
+            mnemonic: MAINNET_MNEMONIC,
+        },
+    },
+},
+  solidity: {
+    compilers: [
+        {
+            version: "0.8.18",
+            settings: {
+                optimizer: {
+                    enabled: true,
+                    runs: 2000,
+                },
+            },
+        },
+    ],
+},
 };
 
 export default config;
