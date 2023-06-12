c96f66fb59aff89322e30201e635f279ae0f63f9
xiaohuo <xiaohuo@voyage.finance>
2023-06-12 14:29:58 +0800

    feat: add mock VRFCoordinator

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,8 +5,28 @@ async function main() {
   await weth.waitForDeployment();
   console.log("WETH Address: ", weth.target);
 
+  // mock coordinator
+  const mockCoordinator = await ethers.deployContract("VRFCoordinatorV2Mock",["100000000000000000","1000000000"]);
+  await mockCoordinator.waitForDeployment();
+  console.log("VRFCoordinatorV2Mock: ", mockCoordinator.target);
+
+  // create subscription
+  await mockCoordinator.createSubscription();
+
+  // check subscription
+  const sub = await mockCoordinator.getSubscription(1);
+  console.log("subscription", sub);
+
+  // fund subscription
+  await mockCoordinator.fundSubscription(1,"1000000000000000000");
+
+  // check subscription again
+  const subAfter = await mockCoordinator.getSubscription(1);
+  console.log("subscription after funding: ", subAfter);
+
   // https://docs.chain.link/vrf/v2/direct-funding/supported-networks
-  const vrfCoordinatorAddress = "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D";
+  // const vrfCoordinatorAddress = "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D";
+  const vrfCoordinatorAddress = mockCoordinator.target;
   const linkTokenAddress = "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D";
   const randomNumberGenerator = await ethers.deployContract("RandomNumberGenerator", [vrfCoordinatorAddress, linkTokenAddress]);
   await randomNumberGenerator.waitForDeployment();
